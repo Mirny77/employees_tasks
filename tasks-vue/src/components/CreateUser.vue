128cefe42bf9a213102f4c9a7622971680751d14
Mirny77 <mironov.ilya20160@yandex.ru>
2020-06-16 20:13:59 +0300

    update

--- tasks-vue/src/components/CreateUser.vue
+++ tasks-vue/src/components/CreateUser.vue
@@ -8,7 +8,7 @@
                     <input type="text" name="name" class="form-control" v-model="user.name" placeholder="ФИО">
 
 
-                 <input type="file" id="img" ref="file" v-on:change="handleFileUpload()"/>
+                    <input type="file" id="img" ref="file" v-on:change="handleFileUpload()"/>
 
                     <div class="form-group">
                         <label for="tasks">Задачи</label>
@@ -27,36 +27,33 @@
 <script>
     import axios from 'axios';
     export default {
-       data(){
-           return{
-               user:{}
-           }
-       },
+        data(){
+            return{
+                user:{}
+            }
+        },
         methods:{
-           createUser(){
-               let formData = new FormData();
-               formData.append('file', this.file);
-               axios.post('http://127.0.0.1:8000/api/users', this.user,formData, {
-                   headers: {
-                       'Content-Type': 'multipart/form-data'
-                   }
-               }).then(response=>{
-                   this.$router.push('/')
-                   console.log(response)
-               }).catch(error=>{
-                   console.log(error)
-               })
-
-           },
+            createUser(){
+                let formData = new FormData();
+                formData.append('file', this.file);
+                axios.post('http://127.0.0.1:8000/api/users', this.user,formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }).then(response=>{
+                    this.$router.push('/')
+                    console.log(response)
+                }).catch(error=>{
+                    console.log(error)
+                })
+            },
             handleFileUpload(){
                 this.file = this.$refs.file.files[0];
             },
-
         }
     }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
---
+++ tasks/app/Traits/Eloquent/Uploadable.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Traits\Eloquent;
+
+use Illuminate\Support\Facades\Storage;
+
+trait Uploadable
+{
+    public function upload($file, $storage = 'public', $folder = 'uploads')
+    {
+        $filename = uniqid() . '_' . str_replace(' ', '_', $file->getClientOriginalName());
+        $img = Storage::disk($storage)->putFileAs($folder, $file, $filename);
+
+        if (Storage::disk($storage)->exists($img)) {
+            return $img;
+        }
+
+        return null;
+    }
+}
